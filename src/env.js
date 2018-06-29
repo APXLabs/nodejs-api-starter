dd4db6c480a73ddff25867746657fac579826f8a
Brian Leppez <brian.leppez@upskill.io>
2018-06-29 17:17:33 -0500

    fixed some comments

--- src/env.js
+++ src/env.js
@@ -3,10 +3,7 @@ import logger from './logger';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-/**
- * `keyblade` will make sure we don't rely on undefined values.
- */
-
+// `keyblade` will make sure we don't rely on undefined values.
 const env = keyblade(process.env, {
   message: key => `${key} not found in the loaded environment`,
   logBeforeThrow: message => logger.error(message),
