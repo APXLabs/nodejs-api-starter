c0a9ef794b9a8c48a24f7bbb41ecfb36d86136d0
Brian Leppez <brian.leppez@upskill.io>
2018-07-12 08:31:23 -0500

    added basic testing

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testMatch: ['**/__tests__/**/*.test.js'],
+  testEnvironment: 'node',
+  collectCoverageFrom: ['src/**/*.js'],
+  coveragePathIgnorePatterns: ['/node_modules/', '__tests__'],
+};
