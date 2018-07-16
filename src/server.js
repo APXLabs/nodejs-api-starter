cee9b79e108b00e7bbfa725d2a8a900844b0eb87
Brian Leppez <brian.leppez@upskill.io>
2018-07-16 10:24:03 -0500

    resolved graceful shutdown

--- src/server.js
+++ src/server.js
@@ -3,21 +3,6 @@
 import createServer from './app';
 import env from './env';
 
-// Launch Node.js server
-createServer().then(
-  app =>
-    app.listen(env.PORT, env.LISTEN_IP, () => {
-      const mode = env.NODE_ENV;
-      console.debug(
-        `Server listening on ${env.LISTEN_IP}:${env.PORT} in ${mode} mode`,
-      );
-    }),
-  err => {
-    console.error('Error while starting up server', err);
-    process.exit(1);
-  },
-);
-
 // Shutdown Node.js app gracefully
 function handleExit(options, err) {
   if (options.cleanup) {
@@ -36,7 +21,21 @@ function handleExit(options, err) {
   if (options.exit) process.exit();
 }
 
-process.on('exit', handleExit.bind(null, { cleanup: true }));
-process.on('SIGINT', handleExit.bind(null, { exit: true }));
-process.on('SIGTERM', handleExit.bind(null, { exit: true }));
-process.on('uncaughtException', handleExit.bind(null, { exit: true }));
+// Launch Node.js server
+createServer().then(
+  app =>
+    app.listen(env.PORT, env.LISTEN_IP, () => {
+      const mode = env.NODE_ENV;
+      console.debug(
+        `Server listening on ${env.LISTEN_IP}:${env.PORT} in ${mode} mode`,
+      );
+      process.on('exit', handleExit.bind(null, { cleanup: true }));
+      process.on('SIGINT', handleExit.bind(null, { exit: true }));
+      process.on('SIGTERM', handleExit.bind(null, { exit: true }));
+      process.on('uncaughtException', handleExit.bind(null, { exit: true }));
+    }),
+  err => {
+    console.error('Error while starting up server', err);
+    process.exit(1);
+  },
+);
